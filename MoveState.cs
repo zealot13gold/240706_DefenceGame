410bb5cb64557208b13733a97e989f2131c1f5a5
zealot13gold <zealot13gold@gmail.com>
2024-07-13 12:58:20 +0900

    Defence_Game
    2024-07-13

---
+++ MoveState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MoveState : BaseState
+{
+    public MoveState(GameObject unit, LayerMask playerState) : base(unit, playerState) { }
+
+    public override void OnStateEnter()
+    {
+
+    }
+
+    public override void OnStateUpdate()
+    {
+
+    }
+    public override void OnStateExit()
+    {
+
+    }
+}
